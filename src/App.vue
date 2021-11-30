126cf750a9b2ba96e7924159ed66e34268a7e4bc
xinfo15 <2608443688@qq.com>
2021-11-30 12:09:20 +0800

    sdfjsd

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,14 @@
       <input type="file" @change="selectFile" />
       <el-button type="primary" @click="uploadChunk">上传</el-button>
     </div>
+    <h2>生成hash</h2>
+    <el-progress class="hash" :percentage="hashPercent"></el-progress>
   </div>
 </template>
 
 <script>
+import hashWoker from './hash.worker.js'
+
 export default {
   name: 'App',
   data() {
@@ -15,25 +19,35 @@ export default {
       // 2M
       chunkLimitSize: 2 * 1024 * 1024,
       chunkList: [],
-      file: null
+      file: null,
+      worker: null,
+      hashPercent: 0,
+      hash: ''
     }
   },
   methods: {
     request(method, url, data) {
-      console.log(arguments)
       return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest()
-        xhr.open('post', 'http://localhost:8888/save_chunk', true)
-        xhr.send(data)
+        xhr.open(method, url, true)
 
         xhr.onreadystatechange = function () {
           if (xhr.readyState === 4) {
-            if ((xhr.status > 200 && xhr.status < 300) || xhr.status === 304) {
+            if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
               resolve(xhr.responseText)
             }
           }
         }
+        xhr.send(data)
       })
+    },
+    selectFile(e) {
+      const [file] = e.target.files
+      if (!file) return
+
+      this.file = file
+      this.chunkList = []
+      this.splitChunks(file)
     },
     splitChunks(file) {
       const totalSize = file.size
@@ -45,25 +59,63 @@ export default {
         chunkList.push(file.slice(start, end))
       }
     },
-    selectFile(e) {
-      const [file] = e.target.files
-      if (!file) return
+    calcContentHash() {
+      return new Promise((resolve, reject) => {
+        this.worker = new hashWoker()
+        const worker = this.worker
 
-      this.file = file
-      this.splitChunks(file)
+        // console.log(worker);
+        const chunkList = this.chunkList
+        worker.postMessage(chunkList)
+        worker.onmessage =  (e) => {
+          const {percent, hash} = e.data
+          this.hashPercent = percent
+          this.hash = hash
+
+          // console.log(percent, hash);
+          if (hash) {
+            resolve()
+          }
+        }
+      })
+    },
+    // 通知服务器合并chunks
+    notifyMergeChunks() {
+      const formData = new FormData()
+      formData.append('hash', this.hash)
+      formData.append('filename', this.file.name)
+      formData.append('size', this.chunkLimitSize)
+      return this.request('post', 'http://localhost:8888/merge_chunks', formData)
+    },
+    async getChunkList() {
+      let cacheChunkList = await this.request('get', 'http://localhost:8888/cache_chunk_list?hash=' + this.hash)
+      const dirs = JSON.parse(cacheChunkList).dirs
+
+      if (dirs) {
+        const cacheIdxList = dirs.map((val, idx) => parseInt(val.split('_')[1]))
+        this.chunkList = this.chunkList.filter((val, idx) => cacheIdxList.indexOf(idx) === -1)
+      }
     },
     async uploadChunk() {
+      await this.calcContentHash()
+
+      await this.getChunkList()
+      console.log(chunkList);
       const chunkList = this.chunkList
 
       const requestList = chunkList.map((val, idx) => {
         const formData = new FormData()
         formData.append('chunk', val)
-        formData.append('filename', this.file.name + '_' + idx)
+        formData.append('idx', idx)
+        formData.append('hash', this.hash)
         return this.request('post', 'http://localhost:8888/save_chunk', formData)
       })
-
       await Promise.all(requestList)
+      console.log('notify');
+      await this.notifyMergeChunks()
     },
+  },
+  created() {
   }
 }
 </script>
--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const fs = require('fs')
 const Multiparty = require('multiparty')
 const path = require('path')
+const { Z_FIXED } = require('zlib')
 
 const app = express()
 const port = 8888
@@ -12,16 +13,72 @@ app.use((req, res, next) => {
   next()
 })
 
+const pipeStream = (path, writeStream) =>
+  new Promise((resolve) => {
+    const readStream = fs.createReadStream(path)
+    readStream.on('end', () => {
+      // fs.unlinkSync(path)
+      resolve()
+    })
+    readStream.pipe(writeStream)
+  })
+
+app.get('/cache_chunk_list', (req, res) => {
+  const hash = req.query.hash
+  let ans
+  if (fs.existsSync(hash)) {
+    ans = {dirs: fs.readdirSync(hash)}
+  } else {
+    ans = {}
+  }
+
+  res.send(JSON.stringify(ans))
+})
+
+app.post('/merge_chunks', (req, res) => {
+  const form = new Multiparty.Form()
+  form.parse(req, async function (err, fields, files) {
+    if (err) return
+    const chunkDir = fields.hash[0]
+    const targetName = fields.filename[0]
+    const size = fields.size[0]
+    if (!chunkDir) return
+
+    if (fs.existsSync(chunkDir)) {
+      const chunkPaths = fs.readdirSync(chunkDir)
+      console.log(chunkPaths);
+      // chunkPaths.sort((a, b) => parseInt(a.split('_')[1]) - parseInt(b.split('_')[1]))
+
+      await Promise.all(
+        chunkPaths.map((chunkPath, idx) => {
+          return pipeStream(
+            `${chunkDir}/${chunkPath}`,
+            fs.createWriteStream(targetName, {
+              start: idx * size,
+              end: (idx + 1) * size,
+            })
+          )
+        })
+      )
+    }
+  })
+  res.end()
+})
+
 app.post('/save_chunk', (req, res) => {
   const form = new Multiparty.Form()
-  form.uploadDir='tmp'
+  form.uploadDir = 'tmp'
   form.parse(req, function (err, fields, files) {
-    console.log(err);
+    // console.log(err);
     if (err) return
-    const filename = fields.filename[0]
+    const idx = fields.idx[0]
+    const hash = fields.hash[0]
     const chunk = files.chunk[0]
-    if (!chunk || !filename) return
-    const dir = filename.split('_')[0]
+
+    if (!chunk || !idx || !hash) return
+
+    const dir = hash
+    const filename = hash + '_' + idx
 
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir)
